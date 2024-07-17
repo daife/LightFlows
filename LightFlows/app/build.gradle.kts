d12cb3a5b340ea28be1291f21e42e77dc75d5e57
dypho <63540280+daife@users.noreply.github.com>
2024-07-17 13:51:31 +0800

    Update build.gradle.kts

--- LightFlows/app/build.gradle.kts
+++ LightFlows/app/build.gradle.kts
@@ -47,9 +47,7 @@ implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
   implementation("org.bytedeco:javacv:1.5.9")
     implementation("org.bytedeco:javacpp-presets:1.5.9")
-    implementation("org.bytedeco:openblas:0.3.23-1.5.9:android-arm")
-    //implementation("org.bytedeco:openblas:0.3.23-1.5.9:android-arm64")
-    //implementation("org.bytedeco:opencv:4.7.0-1.5.9:android-arm")
-    //implementation("org.bytedeco:opencv:4.7.0-1.5.9:android-arm64")
+    implementation("org.bytedeco:openblas:0.3.23-1.5.9:android-arm")//这两行一起编译似乎会报错，可以注释其中一行，分别编译打包，然后将lib整合到一块
+    implementation("org.bytedeco:opencv:4.7.0-1.5.9:android-arm")//
     implementation("com.squareup.okhttp3:okhttp:4.2.2")
 }
